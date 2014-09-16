f073efc6fb9b6a62e675ce47570f05545fe34d8c
Petr Voytsik <voitsik@asc.rssi.ru>
2014-09-16 12:14:41 +0400

    Version bump

--- rdf_aspec.c
+++ rdf_aspec.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011-2013 by Petr Voytsik                               *
+ *   Copyright (C) 2011-2014 by Petr Voytsik                               *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -33,7 +33,7 @@
 
 const char program[] = "rdf_aspec";
 const char author[]  = "Petr Voytsik";
-const char version[] = "2.0";
+const char version[] = "2.1";
 
 
 /* Decoded RDF-file header */
